79ca031288fbc779e34f45be02ab6708424439b0
lakshya <developer.aary@gmail.com>
2025-12-30 06:02:23 +0530

    social links

--- apps/web/src/components/layout/Footer.tsx
+++ apps/web/src/components/layout/Footer.tsx
@@ -10,10 +10,10 @@ export function Footer() {
   ];
 
   const socialLinks = [
-    { name: 'GitHub', icon: Github, href: 'https://github.com' },
-    { name: 'LinkedIn', icon: Linkedin, href: 'https://linkedin.com' },
-    { name: 'Twitter', icon: Twitter, href: 'https://twitter.com' },
-    { name: 'Email', icon: Mail, href: 'mailto:contact@codescriet.com' },
+    // { name: 'GitHub', icon: Github, href: 'https://github.com' },
+    { name: 'LinkedIn', icon: Linkedin, href: 'https://www.linkedin.com/company/code-scriet/' },
+    // { name: 'Twitter', icon: Twitter, href: 'https://twitter.com' },
+    { name: 'Email', icon: Mail, href: 'mailto:code.scriet@gmail.com' },
   ];
 
   return (
